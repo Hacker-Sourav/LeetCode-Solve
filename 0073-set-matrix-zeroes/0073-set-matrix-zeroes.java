ea7c115681502f9339ebdffbfd1d70afd4f5d9e7
Hacker-Sourav <130467385+Hacker-Sourav@users.noreply.github.com>
2024-09-14 23:45:08 +0530

    Time: 0 ms (100%), Space: 45.3 MB (85.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,18 +4,21 @@ class Solution {
         int n = matrix[0].length;
         boolean flag1 = false;
         boolean flag2 = false;
+        // check for col 0 having 0's
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 flag1 = true;
                 break;
             }
         }
+        // check for row 0 having 0's
         for (int j = 0; j < n; j++) {
             if (matrix[0][j] == 0) {
                 flag2 = true;
                 break;
             }
         }
+        // use row 0 and col 0 as the record
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -24,20 +27,15 @@ class Solution {
                 }
             }
         }
+        // modify others by records
         for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < n; j++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        for (int j = 1; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < m; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
+        // modify rest row 0 and col 0 through flags
         if (flag1) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
